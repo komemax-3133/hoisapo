562fc87051a4700a9d6df2af784a115aed45c550
komemax-3133 <vhnyzfwjnm@privaterelay.appleid.com>
2026-09-22 14:01:15 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,1 @@
-const CACHE="hoisapo-v6";
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"]))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener("install",e=>self.skipWaiting());self.addEventListener("activate",e=>self.clients.claim());
